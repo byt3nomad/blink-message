6dedef485a3343590e24159a41fba3a666f5dab3
byt3nomad <byt3nomad>
2024-12-11 00:10:31 +0200

    created basic design without func

--- client/blink/src/App.jsx
+++ client/blink/src/App.jsx
@@ -1,10 +1,21 @@
-import { Avatar } from "@/components/ui/avatar";
+import { Card, Heading, Textarea, VStack } from "@chakra-ui/react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
-      <Avatar></Avatar>
-    </>
+    <VStack px={4} pt={10}>
+      <Card.Root maxW="700px" w="full" size="sm">
+        <Card.Header>
+          <Heading size="md">Message</Heading>
+        </Card.Header>
+        <Card.Body>
+          <Textarea size="md" resize="none" placeholder="Start typing..." />
+        </Card.Body>
+        <Card.Footer justifyContent="flex-end">
+          <Button>Create link</Button>
+        </Card.Footer>
+      </Card.Root>
+    </VStack>
   );
 }
 
