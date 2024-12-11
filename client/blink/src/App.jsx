d29ea31f31df427ef503fe6f488d5d31884af06c
byt3nomad <byt3nomad>
2024-12-11 21:09:27 +0200

    add validations

--- client/blink/src/App.jsx
+++ client/blink/src/App.jsx
@@ -1,19 +1,28 @@
 import { Card, Heading, Textarea, VStack } from "@chakra-ui/react";
 import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
 
 function App() {
   return (
     <VStack px={4} pt={10}>
       <Card.Root maxW="700px" w="full" size="sm">
-        <Card.Header>
-          <Heading size="md">Message</Heading>
-        </Card.Header>
-        <Card.Body>
-          <Textarea size={"lg"} resize="none" placeholder="Start typing..." />
-        </Card.Body>
-        <Card.Footer justifyContent="flex-end">
-          <Button>Create link</Button>
-        </Card.Footer>
+        <form>
+          <Card.Header>
+            <Heading size="md">Message</Heading>
+          </Card.Header>
+          <Card.Body>
+            <Field required>
+              <Textarea
+                size={"lg"}
+                resize="none"
+                placeholder="Start typing..."
+              />
+            </Field>
+          </Card.Body>
+          <Card.Footer justifyContent="flex-end">
+            <Button type="submit">Create link</Button>
+          </Card.Footer>
+        </form>
       </Card.Root>
     </VStack>
   );
